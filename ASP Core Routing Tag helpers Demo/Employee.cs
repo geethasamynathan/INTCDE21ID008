11c554599eab0f1c5dad998904506b4d7e7a648d
geethasamynathan <geethasamynathan2011@gmail.com>
2021-06-22 11:18:36 +0530

    Demo Files Added

---
+++ ASP Core Routing Tag helpers Demo/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemo1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+    }
+    public enum Department
+    {None,
+        It,
+        PayRoll,
+        Admin
+    }
+
+
+}
---
+++ ASP Core Routing Tag helpers Demo/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCDemo1.Models;
+
+namespace MVCDemo1.Controllers
+{
+ 
+  //  [Route("[controller]")]
+    public class EmployeeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+
+        }
+     //  [Route("Index")]
+      // [Route("Employee")]
+      //[Route("[action]")]
+        public IActionResult Index()
+        {
+            return View(_employeeRepository.GetEmployees());
+        }
+
+       // [Route("Details")]
+       //[Route("[action]")]
+        public IActionResult Details(int id)
+        {
+            Employee employee = _employeeRepository.GetEmployeeById(id);
+            return View(employee);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            _employeeRepository.Save(emp);
+            return View("Index",_employeeRepository.GetEmployees());
+        }
+    }
+}
